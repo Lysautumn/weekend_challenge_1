349ea466ccc4c0608381fd9ac198d533ad626309
Ally Boyd <alyssa.boyd85@gmail.com>
2016-10-18 14:53:31 -0500

    Completed challenge

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ app.controller('BasicController', function() {
 
   self.employees = [];
 
+  self.expenditure = 0;
+
+  self.totalExp = function() {
+    self.expenditure = 0;
+    self.employees.forEach(function(employee) {
+      self.expenditure += employee.salary;
+    });
+    console.log(self.expenditure);
+  };
+
   self.createEmployee = function() {
     console.log('Submitted employee:', self.employee);
     self.employees.push(angular.copy(self.employee));
-    calcExp();
+    self.totalExp();
+    self.employee = {};
   };
-  self.expenditure = 0;
-  var calcExp = function() {
-    //go through emp array, get salary info, add together, divide by 12
-    self.expenditure += self.employees.salary / 12;
+  self.deleteEmployee = function($index) {
+    self.employees.splice($index, 1);
+    self.totalExp();
   };
 });
